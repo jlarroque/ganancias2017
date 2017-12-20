f358447f964acc1bea1d631f4eedbde137397566
Cristian Bertelegni <cristianbertelegni@gmail.com>
2017-12-20 17:44:35 -0300

    deploy app

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect'),
   uglify = require('gulp-uglify'),
-  rename = require("gulp-rename");
+  rename = require("gulp-rename"),
+  s3 = require( "gulp-s3" ),
+  fs = require('fs'),
+  path = require('path'),
+  opn = require('opn');
 
 
 /** server */
@@ -44,6 +48,31 @@ gulp.task('copy-bower', function () {
     .pipe(gulp.dest("dist/bower_components"));
 });
 
+gulp.task('build', ['copy-bower', 'html', 'css', 'js']);
 
 /** default */
-gulp.task('default', ['copy-bower', 'html', 'css', 'js', 'connect', 'watch']);
+gulp.task('default', ['build', 'connect', 'watch']);
+
+
+
+
+const credentialsPath = './s3Credentials.json';
+const pathProduction = "http://especialess3.lanacion.com.ar/";
+
+gulp.task('deploy', ['build'], function () {
+
+    var options = {
+            uploadPath: '17/calculadora-ganancias2018/'
+        }
+
+    var s3Credentials = JSON.parse(fs.readFileSync(credentialsPath));
+
+    gulp.src('**', { cwd: "dist" })
+        .pipe(s3(s3Credentials, options))
+        .on('end', function(e){
+            let urlProductionApp = path.join(pathProduction, options.uploadPath);
+            console.log( "Deploy complete on: \n" +  urlProductionApp);
+            opn(urlProductionApp);
+        });
+
+});
